26d9ec6fbc2ad861f83e85b23cb67a6fd7456ea6
Rishabh Mittal <rishabhmittalcoding@gmail.com>
2025-05-18 17:11:34 +0530

    user register route created

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,13 @@ const express =require("express");
 const cors=require('cors');
 const app= express();
 const connectToDB=require('./db/db.js');
+const userRoutes=require('./routes/user.routes.js');
 connectToDB();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use('/users',userRoutes);
 
 app.get('/',(req,res)=>{
     res.send("Hello World");
