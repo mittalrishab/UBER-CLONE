b68332f789015a17c7b3f87f44942076d9c3e1cc
Rishabh Mittal <rishabhmittalcoding@gmail.com>
2025-05-18 17:56:47 +0530

    profile and logout route for user created

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,14 @@ const cors=require('cors');
 const app= express();
 const connectToDB=require('./db/db.js');
 const userRoutes=require('./routes/user.routes.js');
+const cookieParser = require('cookie-parser');
 connectToDB();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
 app.use('/users',userRoutes);
 
 app.get('/',(req,res)=>{
---
+++ backend/middlewares/auth.middlewares.js
@@ -0,0 +1,22 @@
+const userModel=require("../models/user.model.js");
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
+
+module.exports.authUser=async(req,res,next)=>{
+    const token=req.cookie.token || req.headers.authorization?.split(' ')[1];
+    if(!token){
+        return res.status(401).json({message:'Unauthorized'});
+    }
+    const isBlackListed = await userModel.findOne({token:token});
+    if(isBlackListed){
+        res.status(401).json({message:"Unauthorized"});
+    }
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        const user = await userModel.findById(decoded._id);
+        req.user=user;
+        next();
+    }catch(error){
+        return res.status(401).json({message:"Unauthorized"});
+    }
+}
